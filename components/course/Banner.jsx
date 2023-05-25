03e8c38e80e39ca0a5edde388c554b7b69077f82
sagar ratanpara <sagar.ratanpara@hotmail.com>
2023-05-26 02:15:27 +0530

    banner section course list button added

--- components/course/Banner.jsx
+++ components/course/Banner.jsx
@@ -24,23 +24,88 @@ export default function Banner({}) {
             </div>
             <img src="/assets/image 19.png" alt="banner" />
           </Box>
+          <Box className={styles.tabs}>
+            <button className={styles.enrollBtn}>
+              Practicle Training - Offline
+            </button>
+            <button className={styles.primaryBtn}>
+              Practicle Training - Online
+            </button>
+            <button className={styles.primaryBtn}>
+              Individual Training - Online
+            </button>
+            <button className={styles.primaryBtn}>
+              Free Training - Online
+            </button>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
           <Box className={styles.contentBox}>
             <div className={styles.contentBlock}>
-              <h2 className={styles.headingText}>Practical Training Offline</h2>
+              <h2 className={styles.headingText}>Practicle Training Online</h2>
               <button className={styles.enrollBtn}>ENROLL NOW</button>
             </div>
             <img src="/assets/image 19.png" alt="banner" />
           </Box>
+          <Box className={styles.tabs}>
+            <button className={styles.primaryBtn}>
+              Practicle Training - Offline
+            </button>
+            <button className={styles.enrollBtn}>
+              Practicle Training - Online
+            </button>
+            <button className={styles.primaryBtn}>
+              Individual Training - Online
+            </button>
+            <button className={styles.primaryBtn}>
+              Free Training - Online
+            </button>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
-          <Box className={styles.contentBox}> 
+          <Box className={styles.contentBox}>
             <div className={styles.contentBlock}>
-              <h2 className={styles.headingText}>Practical Training Offline</h2>
+              <h2 className={styles.headingText}>Individual Training Online</h2>
               <button className={styles.enrollBtn}>ENROLL NOW</button>
             </div>
             <img src="/assets/image 19.png" alt="banner" />
+          </Box>
+          <Box className={styles.tabs}>
+            <button className={styles.primaryBtn}>
+              Practicle Training - Offline
+            </button>
+            <button className={styles.primaryBtn}>
+              Practicle Training - Online
+            </button>
+            <button className={styles.enrollBtn}>
+              Individual Training - Online
+            </button>
+            <button className={styles.primaryBtn}>
+              Free Training - Online
+            </button>
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box className={styles.contentBox}>
+            <div className={styles.contentBlock}>
+              <h2 className={styles.headingText}>Free Training Online</h2>
+              <button className={styles.enrollBtn}>ENROLL NOW</button>
+            </div>
+            <img src="/assets/image 19.png" alt="banner" />
+          </Box>
+          <Box className={styles.tabs}>
+            <button className={styles.primaryBtn}>
+              Practicle Training - Offline
+            </button>
+            <button className={styles.primaryBtn}>
+              Practicle Training - Online
+            </button>
+            <button className={styles.primaryBtn}>
+              Individual Training - Online
+            </button>
+            <button className={styles.enrollBtn}>
+              Free Training - Online
+            </button>
           </Box>
         </SwiperSlide>
       </Swiper>
