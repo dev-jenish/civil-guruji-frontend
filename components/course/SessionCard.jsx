fc44a367e0157bdc8d3dc4bce632e465aa5b85c3
omkar342 <omkarjadhav095@gmail.com>
2023-08-11 16:07:58 +0530

    Changed the naming of zoom sessions to be displayed.

--- components/course/SessionCard.jsx
+++ components/course/SessionCard.jsx
@@ -16,18 +16,31 @@ export default function SessionCard({ meetingData, setSelectedTopic, fromDetails
     })
   }
 
+  // Check if the start time is today
+  const isToday = startTime.isSame(moment(), "day");
+
   return (
     <div className={styles.sessionCard}>
       <span>
         <AiOutlineVideoCamera id={styles.cameraIcon} />
-        <p>{isLive ? `Ongoing Session (${meetingData?.topic})` : `Upcoming Session (${meetingData?.topic})`}</p>
+        <p>
+          {meetingData?.isEnded
+            ? `Session Completed (${meetingData?.topic})`
+            : isLive
+            ? `Ongoing Session (${meetingData?.topic})`
+            : isToday
+            ? `Today's Session (${meetingData?.topic})`
+            : `Upcoming Session (${meetingData?.topic})`}
+        </p>
       </span>
-      {isLive && (!fromDetails) ? (
+      {isLive && !fromDetails ? (
         <Button onClick={handleJoinMeeting} colorScheme="red" size="xs">
           Join
         </Button>
       ) : (
-        <p id={styles.time}>{ startTime.format('hh:mm A')} • {startTime.format("MMMM DD, YYYY")}</p>
+        <p id={styles.time}>
+          {startTime.format("hh:mm A")} • {startTime.format("MMMM DD, YYYY")}
+        </p>
       )}
     </div>
   );
